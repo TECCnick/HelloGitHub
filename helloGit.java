32d80d468b5a4e8ae10a5729a10919e58a8f1887
Timothy Cada <116432244+TECCnick@users.noreply.github.com>
2026-01-30 23:24:17 +0800

    java coffee

---
+++ helloGit.java
@@ -0,0 +1,11 @@
+/*
+Hello GitHub in Java
+01/30/26
+A part of daily commit
+*/
+
+public class helloGit{
+  public static void main(String[] args) {
+    System.out.println("Hello, GitHub!");
+  }
+}
